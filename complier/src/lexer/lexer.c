e68a9ffbb7abb5f8a84326671c17de5dfdb883c0
Matthew Loder <matthewloder@Matthews-MacBook-Pro.local>
2025-01-29 17:38:53 -0500

    created TokenTypes and token structs

--- complier/src/lexer/lexer.c
+++ complier/src/lexer/lexer.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+#include "lexer.h"
+
 int main(int argc, char **argv) {
   printf("Lexer Test");
   return 0;
--- complier/src/lexer/lexer.h
+++ complier/src/lexer/lexer.h
@@ -1,4 +1,30 @@
 #ifndef LEXER_H_
 #define LEXER_H_
 
+enum TokenType {
+  KEYWORD,     // Compiler specific word
+  IDENT,       // Identifiers
+  LITERAL,     // String literals
+  OPERATORS,   // Arithmatic operators
+  PUNCTUATION, // Punctuation and delimeters
+  CONSTANTS,   // Number constants
+  SPECIAL,     // _ and &
+  REMOVE,      // Whitespace and comments
+  ILLEGAL,     // Unknown lexemes and illegal symbols
+};
+
+typedef struct {
+  char *lexem;
+  TokenType token;
+} Token;
+
+int isKeyword(const char *str);
+int isIdent(const char *str);
+int isLiteral(const char *str);
+int isOperator(const char *str);
+int isPunctuation(const char *str);
+int isConstant(const char *str);
+int isSpecial(const char *str);
+int isRemove(const char *str);
+
 #endif
